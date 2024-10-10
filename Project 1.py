6857b244246bdd2089a7d4f93729ed9826e1d4c2
Azzurrella <135655875+Azzurrella@users.noreply.github.com>
2024-10-10 13:17:02 +0300

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,9 @@
+#Project 1: Georgia + Panagiotis = Anniversary
+
+import datetime
+Panagiotis = "Panagiotis"
+Georgia = "Georgia"
+t = datetime.datetime(2024, 2, 16, 0, 0).strftime('%m/%d/%Y')
+print (Panagiotis, Georgia , t)
+
+
---
+++ Project2.py
@@ -0,0 +1,9 @@
+#Project 2: Georgia + Panagiotis = Relationship Duration 
+
+import datetime
+georgia = "Georgia"
+panagiotis = "Panagiotis"
+x = datetime.datetime (2024, 2, 16)
+relationship_time = datetime.datetime.now() - x
+print (georgia , panagiotis , relationship_time)
+ 

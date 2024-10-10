3944b6f775cb5a6c1109ec2ad5634d5e1aabf858
GeoTheQuack <135655875+Azzurrella@users.noreply.github.com>
2024-10-10 14:40:07 +0300

    Add files via upload

---
+++ Project 6.py
@@ -0,0 +1,4 @@
+MarioCharacters = []
+MarioCharacters.append(input ("Tell me your fav Super Mario Character: "))
+MarioCharacters.append(input ("Tell me your second fav Super Mario Character: "))
+print(MarioCharacters)
